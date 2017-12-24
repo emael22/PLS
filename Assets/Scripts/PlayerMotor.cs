71e00dbc8826fd8af12200538ae6db0f02727393
emael22 <malik.toudert@gmail.com>
2017-12-24 15:15:53 +0100

    Grav Fail
    added colors

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,9 +7,9 @@ public class PlayerMotor : MonoBehaviour {
     private CharacterController controller;
     private Vector3 moveVector;
 
-    private float speed = 10.0f;
+    public float speed = 27.0f;
     private float verticalVelocity = 0.0f;
-    private float gravity = 12.0f;
+    private float gravity;
 
 
 	// Use this for initialization
@@ -21,20 +21,9 @@ public class PlayerMotor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        moveVector = Vector3.zero;
+        //moveVector = Vector3.zero;
 
-        if (controller.isGrounded)
-        {
-            verticalVelocity = -0.05f;
-        }
-        else
-        {
-            verticalVelocity -= gravity * Time.deltaTime;
-        }
-
-        moveVector.x = Input.GetAxisRaw("Horizontal")*speed;
-        moveVector.y = verticalVelocity;
-        moveVector.z = speed;
+        moveVector.x = Input.GetAxis("Horizontal")*speed; 
 
         controller.Move(moveVector*Time.deltaTime);
 		
