c6f4d27cdf5385b01f9ff76f4fa5a4ffc8fa11fa
TOUDERT Malik <malik.toudert@djit.fr>
2017-12-26 16:36:51 +0100

    revert to previous motor

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,30 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMotor : MonoBehaviour {
+public class PlayerMotor : MonoBehaviour
+{
 
     private CharacterController controller;
     private Vector3 moveVector;
 
-    public float speed = 27.0f;
+    private float speed = 10.0f;
     private float verticalVelocity = 0.0f;
-    private float gravity;
+    private float gravity = 12.0f;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         controller = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-        //moveVector = Vector3.zero;
+    // Update is called once per frame
+    void Update()
+    {
 
-        moveVector.x = Input.GetAxis("Horizontal")*speed; 
+        moveVector = Vector3.zero;
 
-        controller.Move(moveVector*Time.deltaTime);
-		
-	}
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.05f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        moveVector.y = verticalVelocity;
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+
+    }
 }
